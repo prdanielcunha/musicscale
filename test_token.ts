3d527ef1481fe8911619301b9e8f031d5eba09d7
prdanielcunha <pastordanielpcunha@gmail.com>
2026-07-18 23:53:33 -0300

    refactor: remove temporary diagnostic scripts
    Clean up codebase by removing development-only diagnostic tools and token testing scripts.

--- test_token.ts
+++
@@ -1,7 +0,0 @@
-import { adminAuth as auth } from './services/firebaseAdmin.js';
-
-async function run() {
-  const token = await auth.createCustomToken('EPymQj34Tof3smPuNd3Z8yM4Cw13');
-  console.log("TOKEN:", token);
-}
-run().then(() => process.exit(0)).catch(e => { console.error(e); process.exit(1); });
