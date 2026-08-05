1e3fdbe2b4121d2b94f3c3b7c36c78cb41227bcf
prdanielcunha <pastordanielpcunha@gmail.com>
2026-08-05 19:57:49 -0300

    refactor: remove temporary patch scripts
    Removed leftover automated patching scripts used during development to maintain a cleaner codebase.

--- test_res.cjs
+++
@@ -1,4 +0,0 @@
-const fs = require('fs');
-let code = fs.readFileSync('tests/unit/apiAiImport.test.ts', 'utf8');
-code = code.replace("expect(res.body.song.metadata.declaredKey).toBe(\"Am\");", "console.log(JSON.stringify(res.body, null, 2));\n    expect(res.body.song.metadata.declaredKey).toBe(\"Am\");");
-fs.writeFileSync('tests/unit/apiAiImport.test.ts', code);
