bfaa3edd87bfe8223abb25881e58753be1e93b4b
prdanielcunha <pastordanielpcunha@gmail.com>
2026-09-14 06:52:44 -0300

    feat: load Premium V2 completion styles

--- index.tsx
+++ index.tsx
@@ -6,6 +6,7 @@ import { AppErrorBoundary } from './components/AppErrorBoundary';
 import { markStartupMetric, incrementStartupCounter, markStartupFailure } from './lib/startupTelemetry';
 import './performance-v2.css';
 import './operational-v2.css';
+import './premium-v2-completion.css';
 
 
 const urlParams = new URLSearchParams(window.location.search);
