5265e66b9abbcf7e422b89692147f3f760d9c399
prdanielcunha <pastordanielpcunha@gmail.com>
2026-09-14 21:17:42 -0300

    style: give mobile Create a restrained premium glow

--- components/layout/BottomNav.tsx
+++ components/layout/BottomNav.tsx
@@ -124,7 +124,8 @@ export const BottomNav: React.FC = () => {
               <React.Fragment key={link.id}>
                 {index === 2 && (
                   <motion.div
-                    className="flex min-w-11 flex-1 justify-center"
+                    data-testid="mobile-create-highlight"
+                    className="relative flex min-w-11 flex-1 justify-center before:pointer-events-none before:absolute before:left-1/2 before:top-[3px] before:h-8 before:w-8 before:-translate-x-1/2 before:rounded-full before:border before:border-indigo-300/[0.18] before:bg-indigo-400/[0.07] before:shadow-[0_0_18px_rgba(129,140,248,0.22)] [&_button]:relative [&_button]:z-10 [&_svg]:drop-shadow-[0_0_5px_rgba(165,180,252,0.55)]"
                     animate={{ scale: isCompact ? 0.92 : 1 }}
                     transition={shouldReduceMotion ? { duration: 0 } : { duration: 0.18 }}
                   >
